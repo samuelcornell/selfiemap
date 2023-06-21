53c7b73cb222ec3cee04859ac30df3e3253b49ef
Samuel Cornell <63439814+samuelcornell@users.noreply.github.com>
2023-06-21 21:32:14 +1000

    x
    xx

--- source/config.js
+++ source/config.js
@@ -1,14 +1,13 @@
 var config = {
-    style: 'mapbox://styles/samuelcornell/clj5a9axd000r01pw0cmk78gq',
+    style: 'mapbox://styles/samuelcornell/clj5kp8ha000v01rj0vvwaekh',
     accessToken: 'pk.eyJ1Ijoic2FtdWVsY29ybmVsbCIsImEiOiJjbGozc2lyNHIwYzllM3JtbGRlOXVsdHk3In0.XaZRC_y8cccqy6aFmfi8XQ',
     showMarkers: false,
     markerColor: '#3FB1CE',
     projection: 'equirectangular',
-    //Read more about available projections here
-    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: true,
     theme: 'dark',
-    use3dTerrain: true, //set true for enabling 3D maps.
+    alignment: 'left',
+    use3dTerrain: true,
     auto: false,
     title: 'Mapping selfie-related incidents in Australia',
     subtitle: 'A map visualisation of all selfie-related deaths and injuries reported by the media in Australia',
@@ -22,14 +21,14 @@ var config = {
             // image: './path/to/image/source.png',
             description: '<span style="font-size:24px">People are dying and getting injured from taking selfies all over the world, and especially in aquatic locations.</span><br><br><span style="font-size:18px"><i>How does Australia compare?</i></span><br><br><i>Mapping Australian Selfiecides</i> | By Samuel Cornell.<br><span style = "color:darkgrey;">(Scroll &#x2913; to explore)</span>',
             location: {
-                center: [44.25362, 0.39152],
-                zoom: 1.85,
-                pitch: 45.00,
+                center: [63.39769, 2.91579],
+                zoom: 1.34,
+                pitch: 6.00,
                 bearing: 0.00
             },
             mapAnimation: 'flyTo',
-            speed: 2,
-            curve: 1,
+            speed: 1.2,
+            curve: 1.42,
             rotateAnimation: true,
             callback: '',
             onChapterEnter: [
@@ -57,24 +56,24 @@ var config = {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Sydney cliff fatality: woman falls to death at popular selfie spot,',
-            image: './images/heli.jpg',
-            description: 'A 27 year old female died after a fall from the sea cliffs at Diamond Head Reserve in Vaucluse on August 17th 2019.',
-            location: {
+            title: '27 year old female died after a fall from the sea cliffs at Diamond Head Reserve in Vaucluse on August 17th 2019.',
+            // image: './images/heli.jpg',
+            description: '<a href="https://www.smh.com.au/national/nsw/woman-dies-at-selfie-spot-in-sydney-s-east-20190817-p52i3p.html">Sydney Morning Herald</a>',
+             location: {
                 center: [151.282771, -33.862684],
                 zoom: 14.90,
                 pitch: 45.00,
-                bearing: -40.00
-            },
+                bearing: -40.00,
+    },
             mapAnimation: 'flyTo',
-            speed: 2,
-            curve: 1,
+            speed: 1.2,
+            curve: 1.42,
             rotateAnimation: true,
             callback: '',
             onChapterEnter: [
                 {
                     layer: 'selfies',
-                    opacity: 0
+                    opacity: 0.33
                 },
                 {
                     layer: 'selfies',
@@ -84,11 +83,11 @@ var config = {
             onChapterExit: [
                 {
                     layer: 'selfies',
-                    opacity: 0
+                    opacity: 0.33
                 },
                 {
                     layer: 'selfies',
-                    opacity: 0
+                    opacity: 0.33
                 }
             ]
         },
@@ -97,32 +96,36 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '19 y/o male falls to death from cliffs at Cape Solander after taking selfie',
-            image: './images/cape.jpg',
-            description: 'https://www.abc.net.au/news/2018-07-24/kurnell-cliff-fall-mormon-missionary-gavin-zimmerman/10029406',
+            // image: './images/cape.jpg',
+            description: '<a href="https://www.abc.net.au/news/2018-07-24/kurnell-cliff-fall-mormon-missionary-gavin-zimmerman/10029406">ABC NEWS</a>',
             location: {
                 center: [151.23158, -34.01690],
-                    zoom: 15.97,
-                    pitch: 45.00,
-                    bearing: -40.00,
+                zoom: 15.97,
+                pitch: 45.00,
+                bearing: -40.00,
             },
             mapAnimation: 'flyTo',
+            speed: 1.2,
+            curve: 1.42,
             rotateAnimation: true,
             callback: '',
             onChapterEnter: [{
                 layer: 'selfies',
-                opacity: 0
+                opacity: 0.33
             },
             {
                 layer: 'selfies',
                 opacity: .33
-            }],
-            onChapterExit: [ {
+            }
+        ],
+        onChapterExit: [
+            {
                 layer: 'selfies',
-                opacity: 0
+                opacity: 0.33
             },
             {
                 layer: 'selfies',
-                opacity: 0
+                opacity: 0.33
             }]
         },
         {
@@ -130,8 +133,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '38 y/o female dies after climbing over lookout barrier to take a selfie',
-            image: './images/grampians.jpg',
-            description: 'https://www.theage.com.au/national/victoria/woman-dies-after-climbing-over-safety-barrier-at-popular-grampians-lookout-20201212-p56my9.html.',
+            ///image: './images/grampians.jpg',
+            description: '<a href="https://www.theage.com.au/national/victoria/woman-dies-after-climbing-over-safety-barrier-at-popular-grampians-lookout-20201212-p56my9.html">The Age</a>',
             location: {
                 center: [142.50361, -37.12310],
                 zoom: 16.72,
@@ -139,17 +142,33 @@ var config = {
                 bearing: -40.00
             },
             mapAnimation: 'flyTo',
+            speed: 1.2,
+            curve: 1.42,
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [{
+                layer: 'selfies',
+                opacity: 0.33
+            },
+            {
+                layer: 'selfies',
+                opacity: .33
+            }],
+            onChapterExit: [{
+                layer: 'selfies',
+                opacity: 0.33
+            },
+            {
+                layer: 'selfies',
+                opacity: 0.33
+            }]
         },
         {
             id: 'Second_Vaucluse',
             alignment: 'right',
             hidden: false,
             title: 'Second Selfie Death at Vaucluse',
-            image: './images/vaucluse2.jpg',
+           /// image: './images/vaucluse2.jpg',
             description: 'In 2020, a second person died at Diamond Bay Reserve in Vauculuse after falling from the cliffs while posing for a selfie. 21 y/o female.',
             location: {
                 center: [151.282771, -33.862684],
@@ -160,8 +179,22 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [{
+                layer: 'selfies',
+                opacity: 0.33
+            },
+            {
+                layer: 'selfies',
+                opacity: .33
+            }],
+            onChapterExit: [{
+                layer: 'selfies',
+                opacity: 0.33
+            },
+            {
+                layer: 'selfies',
+                opacity: 0.33
+            }]
         }
     ]
 };

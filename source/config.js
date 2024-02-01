05720fd2c5ffbc9abf49579f1a8bbd20d669223f
Samuel Cornell <63439814+samuelcornell@users.noreply.github.com>
2024-02-01 19:03:14 +1100

    Add files via upload

--- source/config.js
+++ source/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/samuelcornell/cls2u2w1100bj01r60wq07whw',
+    style: 'mapbox://styles/samuelcornell/cls2wp41400d501pocg0x78u4',
     accessToken: 'pk.eyJ1Ijoic2FtdWVsY29ybmVsbCIsImEiOiJjbGozc2lyNHIwYzllM3JtbGRlOXVsdHk3In0.XaZRC_y8cccqy6aFmfi8XQ',
     showMarkers: false,
     markerColor: '#3FB1CE',
@@ -9,8 +9,8 @@ var config = {
     alignment: 'left',
     use3dTerrain: true,
     auto: false,
-    title: 'Visual ‘scrollytelling’: Mapping selfie-related incidents in Australia to identify the geographical problem for emergency services',
-    subtitle: 'A map visualisation of selfie-related incidents reported by the media in Australia',
+    title: 'Mapping selfie and social media-related incidents in Australia',
+    subtitle: '',
     byline: 'By Samuel Cornell',
     footer: 'Source: News Reports. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template. <a href="https://www.jmir.org/2023/1/e47202/PDF">Selfie-Related Incidents: Narrative Review and Media Content Analysis</a>' ,
     chapters: [
@@ -19,7 +19,7 @@ var config = {
             alignment: 'left',
             title: 'An overview',
             // image: './path/to/image/source.png',
-            description: '<span style="font-size:24px">People are dying and getting injured from taking selfies all over the world, and especially in aquatic locations.</span><br><br><span style="font-size:18px"><i>How does Australia compare?</i></span><br><br><i>Mapping Australian Selfie incidents</i> | By Samuel Cornell.<br><span style = "color:darkgrey;">(Scroll &#x2913; to explore)</span>',
+            description: '<span style="font-size:24px">People are dying and getting injured from taking selfies, and social media misadventure, all over the world, and especially in aquatic locations.</span><br><br><span style="font-size:18px"><i>How does Australia compare?</i></span><br><br><i>Mapping Australian Selfie incidents</i> | By Samuel Cornell.<br><span style = "color:darkgrey;">(Scroll &#x2913; to explore)</span>',
             location: {
                 center: [63.39769, 2.91579],
                 zoom: 2.34,
@@ -56,7 +56,7 @@ var config = {
             id: 'Albany',
             alignment: 'left',
             hidden: false,
-            title: 'In 2018, a student fell to his death from a cliff while taking photos at a popular tourist attraction in Australia.The 20 year-old was reportedly posing for selfies with friends when he slipped off the 40-metre precipice at The Gap, near the historic port town of Albany.',
+            title: 'In 2018, a student fell to his death from a cliff while taking photos at a popular tourist attraction in Australia. The 20 year-old was reportedly posing for selfies with friends when he slipped off the 40-metre precipice at The Gap, near the port town of Albany.',
             // image: './images/heli.jpg',
             description: '<a href="https://www.independent.co.uk/news/world/australasia/student-selfie-fall-death-cliff-gap-albany-australia-a8359546.html">The Independent</a>',
              location: {
@@ -95,7 +95,7 @@ var config = {
             id: 'Figure8Pools',
             alignment: 'right',
             hidden: false,
-            title: 'Numerous injurues have occurred at Figure 8 Pools over a number of years. In this case a 22 year old International student was washed off the rocks and subsequently drowned.',
+            title: 'Numerous injuries have occurred at the Figure Eight Pools in the Royal National Park of New South Wales over a number of years. In this case a 22 year-old international student was washed off the rocks and subsequently drowned.',
             // image: './images/heli.jpg',
             description: '<a href="https://www.abc.net.au/news/2019-02-28/call-to-close-access-to-social-media-favourite-figure-8-pools/10853854">ABC News</a>',
              location: {
@@ -125,7 +125,7 @@ var config = {
                     opacity: 0.33
                 },
                 {
-                    layer: 'heatmap,
+                    layer: 'heatmap',
                     opacity: 0.33
                 }
             ]
@@ -134,7 +134,7 @@ var config = {
             id: 'Vaucluse1',
             alignment: 'left',
             hidden: false,
-            title: 'in 2019, a 27 year old female died after a fall from the sea cliffs at Diamond Head Reserve in Vaucluse.',
+            title: 'In 2019, a 27 year-old female died after a fall from the sea cliffs at Diamond Head Reserve in Vaucluse, Sydney.',
             // image: './images/heli.jpg',
             description: '<a href="https://www.smh.com.au/national/nsw/woman-dies-at-selfie-spot-in-sydney-s-east-20190817-p52i3p.html">Sydney Morning Herald</a>',
              location: {
@@ -173,7 +173,7 @@ var config = {
             id: 'Cape_Solander',
             alignment: 'right',
             hidden: false,
-            title: 'In 2018, a 19 year old male fell to his death from cliffs at Cape Solander after taking selfie',
+            title: 'In 2018, a 19 year-old male fell to his death from cliffs at Cape Solander near Sydney after taking selfie',
             // image: './images/cape.jpg',
             description: '<a href="https://www.abc.net.au/news/2018-07-24/kurnell-cliff-fall-mormon-missionary-gavin-zimmerman/10029406">ABC NEWS</a>',
             location: {
@@ -210,7 +210,7 @@ var config = {
             id: 'Grampians',
             alignment: 'left',
             hidden: false,
-            title: 'In 2020, A 38 year old female died after climbing over lookout barrier in The Grampians to take a selfie',
+            title: 'In 2020, A 38 year-old female died after climbing over lookout barrier in The Grampians in Victoria to take a selfie',
             ///image: './images/grampians.jpg',
             description: '<a href="https://www.theage.com.au/national/victoria/woman-dies-after-climbing-over-safety-barrier-at-popular-grampians-lookout-20201212-p56my9.html">The Age</a>',
             location: {
@@ -278,7 +278,7 @@ var config = {
             id: 'Paradise_Caves_Fall',
             alignment: 'left',
             hidden: false,
-            title: 'Paradise Caves Fall. An American tourist falls while taking photos and sustains injuries.',
+            title: 'An American tourist fell while taking photos and sustained injuries at Paradise Caves near Brisbane.',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://7news.com.au/news/qld/woman-falls-5m-from-cliff-of-paradise-caves-at-sunshine-beach-near-noosa-c-11521583">American tourist falls while taking picture from cliff at Paradise Caves at Sunshine Beach</a>',
             location: {
@@ -311,7 +311,7 @@ var config = {
             id: 'Vaucluse_fall_injury',
             alignment: 'right',
             hidden: false,
-            title: 'Vaucluse fall - a notorious spot. A 15 year old female falls and sustains injuries but survives.',
+            title: 'Another Vaucluse incident. A 15 year-old female falls and sustains injuries but survives.',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://www.9news.com.au/national/teenage-girl-falls-from-deadly-selfie-spot-diamond-bay-reserve-sydney-vaucluse/5874baa1-fc17-4539-b694-12cb557bb3bb">9 News.</a>',
             location: {
@@ -344,7 +344,7 @@ var config = {
             id: 'Gibfalls1',
             alignment: 'left',
             hidden: false,
-            title: 'A 19 year old male fell from Gibralrar Falls in the ACT and died. Attributed to Instagram photos.',
+            title: 'A 19 year-old male fell from Gibralrar Falls in the Australian Capital Territory and died. The incident was attributed to Instagram photos.',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://7news.com.au/news/act/19-year-old-man-found-dead-at-gibraltar-falls-identified-c-9772754">7 News.</a>',
             location: {
@@ -377,7 +377,7 @@ var config = {
             id: 'Brisbane',
             alignment: 'right',
             hidden: false,
-            title: 'A 33 year old female was found dead at the base of the popular Kangaroo Point cliffs in Brisbane may have fallen after losing her balance while taking a selfie.',
+            title: 'A 33 year-old female was found dead at the base of the popular Kangaroo Point cliffs in Brisbane and may have fallen after losing her balance while taking a selfie.',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://7news.com.au/news/qld/brazilian-woman-who-fell-from-brisbanes-kangaroo-point-cliffs-may-have-been-taking-selfie-c-3343974">7 News.</a>',
             location: {
@@ -410,7 +410,7 @@ var config = {
             id: 'Gibfalls2',
             alignment: 'left',
             hidden: false,
-            title: 'A 22 year old male was found unconscious in the water and was unable to be revived',
+            title: 'A 22 year-old male was found unconscious in the water and was unable to be revived. This incident came only one week after a previous incident at the same location.',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://7news.com.au/news/act/man-who-died-falling-from-gibraltar-falls-identified-as-22-year-old-patrick-prevett-c-9809482">7 News.</a>',
             location: {
@@ -443,7 +443,7 @@ var config = {
             id: 'Josephine',
             alignment: 'center',
             hidden: false,
-            title: 'Three person rescue of three young males trapped in the water at Josephine Falls',
+            title: 'A three-person rescue of three young males trapped in the water at Josephine Falls',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://au.news.yahoo.com/aussies-fume-over-dangerous-mistake-at-deadly-waterfall-theres-always-one-064337596.html">Yahoo News.</a>',
             location: {
@@ -476,7 +476,7 @@ var config = {
             id: 'Cedar_Creek',
             alignment: 'right',
             hidden: false,
-            title: 'Man, 22, airlifted to hospital after falling 10m from waterfall at Cedar Creek on the Gold Coast',
+            title: 'A male aged 22 was airlifted to hospital after falling 10m from waterfall at Cedar Creek on the Gold Coast, even though the national park was closed due to severe storms. Cedar Creek Falls is a notorious location with much of its visitation driven by social media.',
            /// image: './images/vaucluse2.jpg',
             description: '<a href="https://7news.com.au/news/man-22-airlifted-to-hospital-after-falling-10m-from-waterfall-at-cedar-creek-on-the-gold-coast-c-13147411">7 News.</a>',
             location: {

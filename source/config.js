9a6f77af4c617b7345854c3d2374a46cfd6aa38e
Samuel Cornell <63439814+samuelcornell@users.noreply.github.com>
2025-06-04 09:33:06 +1000

    Update config.js
    update map style

--- source/config.js
+++ source/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/samuelcornell/clzvz10c2000m01pne6pfer85',
+    style: 'mapbox://styles/samuelcornell/cm0g4dhe1010y01pn3ysk6xri',
     accessToken: 'pk.eyJ1Ijoic2FtdWVsY29ybmVsbCIsImEiOiJjbTdrMGd4b2MwY3AzMnFweHB1bmdnaTd1In0.RP2eRag2RHMe9j-FWjXr5Q',
     showMarkers: false,
     markerColor: '#3FB1CE',
